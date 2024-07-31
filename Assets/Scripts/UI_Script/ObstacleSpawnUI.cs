adc9bca16bb0197224071171274bbf934d1ca693
z920804tw <129352148+z920804tw@users.noreply.github.com>
2024-07-31 15:12:11 +0800

    2024/7/31
    1.將關卡一的場景製作完成
    2.有使用到了LOD來渲染近距離和遠距離的物件顯示方式，這樣可以減少GPU的性能消耗。

---
+++ Assets/Scripts/UI_Script/ObstacleSpawnUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ObstacleSpawnUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TMP_Dropdown tMP_Dropdown;
+    public TMP_Text moneyText;
+    public Transform spawnTransform;
+    public GameObject[] spawnPrefabs;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
